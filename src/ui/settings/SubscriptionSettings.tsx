6a8af9bb4765c68b69d73efedf9268af0ef212dc
Will Lynch <will.lynch91@gmail.com>
2020-12-11 21:21:13 -0500

    fix stuck loading for subscription settings #patch

--- src/ui/settings/SubscriptionSettings.tsx
+++ src/ui/settings/SubscriptionSettings.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Divider from '@material-ui/core/Divider';
 
 import { listRentals, Rental } from "makerspace-ts-api-client";
 
@@ -30,7 +31,7 @@ const SubscriptionSettings: React.FC<Props> = () => {
     const subs = rentals.filter(rental => !!rental.subscriptionId);
     setSubscriptionsLoading(curr => ({
       ...curr,
-      ...subs.reduce((states, sub) => ({ ...states, [sub.id]: true }), {})
+      ...subs.reduce((states, sub) => ({ ...states, [sub.subscriptionId]: true }), {})
     }))
     setSubRentals(subs);
   }, [rentals]);
@@ -44,19 +45,25 @@ const SubscriptionSettings: React.FC<Props> = () => {
   return (
     <>
       {isLoading && <LoadingOverlay id="subscription-settings" contained={true}/>}
-      <Grid container style={{ ...isLoading && { display: "none" } }}>
+      <Grid container spacing={8} style={{ ...isLoading && { display: "none" } }}>
         <Grid item xs={12}>
           <Typography variant="h4" gutterBottom={true}>Membership</Typography>
             <SubscriptionDetails onLoad={reportLoad(subscriptionId)} subscriptionId={subscriptionId} /> 
         </Grid>
         
 
-        {!!subRentals.length && <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom={true}>Membership</Typography>
-        </Grid>}
+        {!!subRentals.length && (
+          <>
+            <Grid item xs={12}>
+              <Divider />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h4" gutterBottom={true}>Rentals</Typography>
+            </Grid>
+          </>
+        )}
         {subRentals.map(({ subscriptionId: rentalSubId }) => (
-          <Grid item xs={12}>
-            <Typography variant="h4" gutterBottom={true}>Membership</Typography>
+          <Grid key={rentalSubId} item xs={12}>
             <SubscriptionDetails onLoad={reportLoad(rentalSubId)} subscriptionId={rentalSubId} />
           </Grid>
         ))}
