27c47f1a8b9dbcd7c93f4833a887d6f65994d23c
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-03-15 01:18:36 -0400

    Fix console log formatting in integration.js #patch

--- scripts/integration.js
+++ scripts/integration.js
@@ -76,10 +76,10 @@ const integrationTest = async () => {
       console.log(`Starting test`);
       runCmd(`RAILS_DIR=${railsFolder} PORT=3035 yarn e2e`, reactLogs, endProcess);
     };
-    const startReact = () => {
-      console.log(" Waiting on http://localhost:${port}...");
+    const startReact = () => { 
+      console.log(`Waiting on http://localhost:${port}`);
       //const slept = execSync('sleep 10', { encoding: 'utf8', stdio: 'inherit' });
-      const waiter = execSync("npx wait-on --timeout 20000 http://localhost:${port}", { encoding: 'utf8', stdio: 'inherit' });
+      const waiter = execSync(`npx wait-on --timeout 20000 http://localhost:${port}`, { encoding: 'utf8', stdio: 'inherit' });
       console.log(waiter);
       process.chdir(reactFolder);
       console.log(`Starting React`);
